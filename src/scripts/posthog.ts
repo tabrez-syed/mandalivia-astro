aa3da3830312dbf798a7f54f002d5b6c7ffa519b
Tabrez <tabrez.syed@gmail.com>
2024-10-30 19:14:13 -0500

    adding stuff to the add-mdx-engine branch

---
+++ src/scripts/posthog.ts
@@ -0,0 +1,6 @@
+import posthog from 'posthog-js'
+
+posthog.init('phc_CP6zuGDddEvkptcNvAYDhFMCNuSlL5OxGNwqCYUoxgi', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only'
+}); 
